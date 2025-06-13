448e1e6923f9abc6be08651d5a13e5362103525c
baseerx <bsronnet@gmail.com>
2025-06-13 16:26:28 +0500

    how to add tools to be used by agents for latest results

--- app/agent.py
+++ app/agent.py
@@ -1,5 +1,5 @@
 from google.adk.agents import Agent
-
+import yfinance as yf
 
 basic_agent = Agent(
     model='gemini-2.0-flash-001',
@@ -8,4 +8,23 @@ basic_agent = Agent(
     instruction='Answer user questions to the best of your knowledge',
 )
 
-root_agent = basic_agent
+
+#2.Creating tool agent
+
+def get_stock_price(ticker: str) -> float:
+    """
+    Get the current stock price for a given ticker symbol. this is tool and will be used by the agent to fetch stock prices.
+    """
+    stock = yf.Ticker(ticker)
+    price = stock.info.get('currentPrice', "Price not available")
+    return {"price": price, "ticker": ticker}
+
+tool_agent = Agent(
+    model='gemini-2.0-flash-001',
+    name='tool_agent',
+    description='A tool agent for fetching stock prices.',
+    instruction='Use this agent to get stock prices for a given ticker symbol.',
+    tools=[get_stock_price]
+)
+
+root_agent = tool_agent
--- app/readme.txt
+++ app/readme.txt
@@ -3,3 +3,6 @@ adk web
 adk run app
 
 from google-adk-agents import Agent
+
+
+by adding we can get real time data
